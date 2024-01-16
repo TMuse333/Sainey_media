fd34c922b21c1a8db5e06bb4afa1e24de51a1f98
Thomas Musial <th896493@dal.ca>
2024-01-16 18:02:11 -0400

    added some rad animations

---
+++ .history/src/components/content_20240116161459.jsx
@@ -0,0 +1,135 @@
+import React, {useEffect,useRef,useState} from "react";
+import '../styles/content.css'
+import q3 from '../media/q3-visuals-logo-2.svg'
+import sample from '../media/image-holder.jpg'
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const Content = ({img, title, description, rev,link,linkText,hasTitle,id}) => {
+
+    const [isAnimated, setIsAnimated] = useState(false)
+
+    const contentRef = useRef();
+
+    useEffect(() => {
+      const handleScroll = () => {
+        const contentElement = contentRef.current;
+  
+        // Check if the element is in the viewport
+        const elementTop = contentElement.getBoundingClientRect().top;
+        const elementBottom = contentElement.getBoundingClientRect().bottom;
+  
+        // You can adjust the offset based on your specific needs
+        const offset = 350;
+  
+        if (elementTop < window.innerHeight - offset && elementBottom > offset) {
+          console.log(`Element with ID ${id} is in the viewport!`);
+          setIsAnimated(true)
+        }
+      };
+  
+      // Attach the scroll event listener
+      window.addEventListener("scroll", handleScroll);
+  
+      // Clean up the event listener on component unmount
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }, [id]);
+  
+  
+
+const animations = {
+    hidden: {
+        opacity:0
+    },
+    visible: {
+        opacity:1,
+        transition:{
+            duration:1.5
+        }
+    }
+}
+
+
+const textAnimations = {
+    hidden: {
+        opacity:0,
+        x:-50
+    },
+    visible: {
+        opacity:1,
+        transition:{
+            duration:1.5
+        }
+    }
+}
+    
+
+
+    return (
+        <div
+         className="content-container"
+          ref={contentRef}
+>
+            <div className={`image-text-box ${rev ? 'reverse' : ''}`}>
+
+                {rev ? (
+                    // Render description-box before image-box if rev is true
+                    <div className="description-box ">
+                       {hasTitle && <h2 className="title-text">title</h2>}
+                        <p className='description-text'>
+                      
+
+                          
+                            {link != null && 
+                     <Link to={link}>
+                     <button className="button">{linkText}</button>
+                  </Link>
+                     }    
+                         
+                        </p>
+                    </div>
+                ) : null}
+
+
+                <div className="image-box">
+                    <h1 className="title-text">{title}</h1>
+                    <motion.img
+                     src={sample}
+                      loading="lazy"
+                       className='content-image'
+                       initial={animations.hidden}
+                       animate={isAnimated? animations.visible : animations.hidden}
+                       />
+                </div>
+
+                {rev ? null : (
+                    // Render description-box after image-box if rev is false
+                    <div className="description-box">
+                         {hasTitle && <h2 className="title-text">title</h2>}
+                        <p className='description-text'>
+                         
+                            {description}
+
+                            <br/>
+                     {link != null && 
+                     <Link to={link}>
+                     <button className="button">{linkText}</button>
+                  </Link>
+                     }    
+                        </p>
+                    </div>
+                )}
+                
+
+            </div>
+
+             
+
+           
+        </div>
+    );
+}
+
+export default Content;
---
+++ .history/src/components/testimonials_20240116175749.jsx
@@ -0,0 +1,295 @@
+import React, { useState,useRef,useEffect } from 'react';
+import { Carousel } from 'react-bootstrap';
+
+import dunk from '../media/best_dunk.mp4';
+
+import oneLeg from '../media/one-leg-dunk.mp4'
+import vert from '../media/standing-2-hander.mp4'
+import '../styles/testimonials.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
+
+const Testimonials = () => {
+  const videos = [
+    {
+      id: 1,
+      title: 'The Quantum Card Game',
+      src: oneLeg,
+      credit: 'TMuse',
+    },
+    {
+      id: 2,
+      title: 'Dunking',
+      src: dunk,
+      credit: 'TMuse',
+    },
+    {
+      id:3,
+      title:'offvert',
+      src:vert,
+      credit: 'TMuse',
+    }
+  ];
+
+  const descriptions = [
+  
+  
+    {
+      name:'Escalade',
+      description:`"He is pure, give him your money"`
+    },
+    {
+      name:'Lujain James',
+      description:`"I make more back than i spend"`
+    },
+    {
+      name:'Aboubacar',
+      description:`"Wow he is such a real one"`
+
+    },
+
+   
+    
+  ]
+
+  const [isMobile, setIsMobile] = useState(true);
+
+  // useEffect to update the state based on screen width
+  useEffect(() => {
+    const handleResize = () => {
+      // Check the screen width and update the state
+      setIsMobile(window.innerWidth < 600);
+    };
+
+    // Initial check on component mount
+    handleResize();
+
+    // Attach the event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup function to remove the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []); 
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(true);
+
+  const handleSlide = (index) => {
+    const scrollPosition = window.scrollY +300;
+
+    setCurrentIndex(index);
+    setIsPlaying(true);
+  
+   
+    const style = {
+      width:'20vw'
+    }
+
+      // window.scrollTo(-10, scrollPosition);
+
+    
+  };
+
+  const [isAnimated, setIsAnimated] = useState(false)
+
+  const contentRef = useRef();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const contentElement = contentRef.current;
+
+      // Check if the element is in the viewport
+      const elementTop = contentElement.getBoundingClientRect().top;
+      const elementBottom = contentElement.getBoundingClientRect().bottom;
+
+      // You can adjust the offset based on your specific needs
+      const offset = 350;
+
+      if (elementTop < window.innerHeight - offset && elementBottom > offset) {
+        // console.log(`Element with ID ${} is in the viewport!`);
+        setIsAnimated(true)
+        console.log('in range!!')
+      }
+    };
+
+    // Attach the scroll event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, ['testimonial']);
+
+  
+
+
+  const animations = {
+    hidden:{
+      width:'0%',
+      opacity:0,
+      transition:
+      {duration:3}
+
+    },
+    visible:{
+      width:'100%',
+      x:0,
+      opacity:1,
+      transition:
+      {duration:3}
+    }
+  }
+
+const buttonAnimation = {
+  initial:{
+    opacity:0
+  },
+  visible:{
+    opacity:1,
+    transition:{
+      delay:2,
+      duration:0.6
+    }
+  }
+}
+  
+const style = {
+  width:'80vw',
+  backgroundColor:
+  '#242424',
+  height: isMobile ?'45%' : '17%',
+  // height:'30%',
+  position:'absolute',
+  left: isAnimated ? '100%': '10%',
+  transition:'width 1.7s ease-in, left 2s ease-in',
+
+  
+  
+}
+
+
+  return (
+    <div className='testimonial-container'
+    id='testimonial'
+    ref={contentRef}>
+
+      <div className='testimonial-text-box'>
+
+      
+
+  <div className='image-box'>
+
+  <motion.div
+  // initial={animations.hidden}
+  // animate={isAnimated ? animations.visible : animations.hidden}
+   className='testimonial-intro'>
+    <motion.h1
+     className='testimonial-name'
+     
+     >
+      Testimonials
+    </motion.h1>
+    <div className='blocker'
+    style={style}>
+
+    </div>
+    <p className='description-text'>
+    Don't just take our word for it. Listen to what our valued clients have to say about their experience with Sainey Media. Their stories are a testament to the impact we've made in helping businesses thrive in the digital world.
+
+    </p>
+    <Link to='portfolio'>
+
+    
+    <motion.button
+    initial={buttonAnimation.initial}
+    animate={isAnimated ? buttonAnimation.visible : buttonAnimation.initial}
+     className='light-button '
+    style={{
+      marginTop:'1rem'
+    }}>
+      view whole portfolio
+    </motion.button> 
+    </Link>
+  
+  </motion.div>
+
+
+
+
+
+
+
+      <Carousel style={{
+        opacity:0
+      }}
+       interval={null} activeIndex={currentIndex}
+     
+        onSelect={(index, e) => {
+          e.preventDefault(); // Prevent the default action
+          handleSlide(index);
+        }}
+      
+      wrap={true}>
+        {videos.map((video, index) => (
+          <Carousel.Item key={video.id}>
+            {currentIndex === index && ( // Render the ReactPlayer only for the current index
+              <video className='testimonial-video'
+              //  width='300px' 
+              //  height='500px'
+                controls>
+                <source src={video.src} type="video/mp4"/>
+              </video>
+            )}
+          </Carousel.Item>
+        ))}
+      </Carousel>
+      </div>
+
+  
+
+    
+
+      {/* <div className='testimonial-box'>
+        <AnimatePresence>
+          <motion.h2 className='testimonial-name'
+          key={currentIndex}
+          initial={{opacity:0}}
+          animate={{ opacity: 1, 
+            transition: { delay: 0.7 } 
+          }}
+          exit={{transition:'all 0.3s ease-in',
+        opacity:0}}
+          >
+            {descriptions[currentIndex].name}
+          </motion.h2>
+
+        </AnimatePresence>
+        <AnimatePresence>
+          <motion.p className='description-text'
+          key={currentIndex}
+          initial={{opacity:0}}
+          animate={{ opacity: 1, 
+            transition: { delay: 1 } 
+          }}
+          exit={{transition:'all 0.3s ease-in',
+        opacity:0}}
+          >
+          {descriptions[currentIndex].description}
+          </motion.p>
+
+        </AnimatePresence>
+      </div> */}
+
+      </div>
+      
+    </div>
+  );
+};
+
+export default Testimonials;
